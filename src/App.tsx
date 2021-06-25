67b834dc54f6c3770dd38bbfdfd5fb2d1592b623
vinassefranche <vincent.francois@inato.com>
2021-06-25 14:43:03 +0200

    Starting the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game/>
     </div>
   );
 }
 
 export default App;
+
+
+const Game = () => {
+  const [xPosition, setXPosition] = useState(0);
+  const [yPosition, setYPosition] = useState(50);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newXPosition = (xPosition + 1) % 95;
+      setXPosition(newXPosition);
+      if(newXPosition === 0) {
+        setYPosition(Math.round(10 + Math.random() * 80))
+      }
+    }, 10);
+    return () => clearInterval(interval)
+  }, [xPosition, setXPosition, setYPosition])
+  return (
+    <div className="game">
+      <img src="/Sammy_punk.png" alt="Sammy the punk shark" className="shark"/>
+      <img src="/water-ball.gif" alt="Water ball" className="water-ball" style={{right: `${xPosition}%`, top: `calc(${yPosition}% - 40px)`}}/>
+    </div>
+  );
+}
