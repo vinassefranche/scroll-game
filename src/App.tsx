be3334e90641115c290ad34fdc3f906935efd8ef
vinassefranche <vincent.francois@inato.com>
2021-06-25 15:43:58 +0200

    Add another ball

--- src/App.tsx
+++ src/App.tsx
@@ -21,22 +21,30 @@ export default App;
 
 const Game = ({setGameOver}: {setGameOver: () => void}) => {
   const [xPosition, setXPosition] = useState(0);
-  const [yPosition, setYPosition] = useState(50);
+  const [yPosition, setYPosition] = useState(33);
+  const [xPositionSlow, setXPositionSlow] = useState(0);
+  const [yPositionSlow, setYPositionSlow] = useState(66);
   const [speed, setSpeed] = useState(1);
+  const [speedSlow, setSpeedSlow] = useState(0.5);
+
   useEffect(() => {
     const interval = setInterval(() => {
+      const newXPosition = (xPosition + speed) % 95;
+
       const shark = document.querySelector('#shark');
       const waterBall = document.querySelector('#water-ball');
-      if(shark && waterBall) {
+      if(newXPosition > 50 && shark && waterBall) {
         const sharkRect = shark.getBoundingClientRect();
         const waterBallRect = waterBall.getBoundingClientRect();
-        if(waterBallRect.top < sharkRect.bottom && waterBallRect.bottom > sharkRect.top
-          && waterBallRect.left < sharkRect.right && waterBallRect.right > sharkRect.left ) {
-            console.log('collision')
+        if(
+          waterBallRect.top < sharkRect.bottom
+          && waterBallRect.bottom > sharkRect.top
+          && waterBallRect.left < sharkRect.right 
+          && waterBallRect.right > sharkRect.left
+          ) {
             setGameOver();
           }
       }
-      const newXPosition = (xPosition + speed) % 95;
       setXPosition(newXPosition);
       if(newXPosition === 0) {
         setYPosition(Math.round(10 + Math.random() * 80))
@@ -46,10 +54,38 @@ const Game = ({setGameOver}: {setGameOver: () => void}) => {
     return () => clearInterval(interval)
   }, [xPosition, setXPosition, setYPosition, setGameOver, speed, setSpeed]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newXPosition = (xPositionSlow + speedSlow) % 95;
+
+      const shark = document.querySelector('#shark');
+      const waterBall = document.querySelector('#water-ball-slow');
+      if(newXPosition > 50 && shark && waterBall) {
+        const sharkRect = shark.getBoundingClientRect();
+        const waterBallRect = waterBall.getBoundingClientRect();
+        if(
+          waterBallRect.top < sharkRect.bottom
+          && waterBallRect.bottom > sharkRect.top
+          && waterBallRect.left < sharkRect.right 
+          && waterBallRect.right > sharkRect.left
+          ) {
+            setGameOver();
+          }
+      }
+      setXPositionSlow(newXPosition);
+      if(newXPosition === 0) {
+        setYPositionSlow(Math.round(10 + Math.random() * 80))
+        setSpeedSlow(Math.random() > 0.5 ? speedSlow : Math.min(5, speedSlow + 0.25))
+      }
+    }, 20);
+    return () => clearInterval(interval)
+  }, [xPositionSlow, setXPositionSlow, setYPositionSlow, setGameOver, speedSlow, setSpeedSlow]);
+
   return (
     <div className="game">
       <img src="/Sammy_punk.png" alt="Sammy the punk shark" className="shark" id="shark"/>
       <img src="/water-ball.gif" alt="Water ball" className="water-ball" style={{right: `${xPosition}%`, top: `calc(${yPosition}% - 40px)`}} id="water-ball"/>
+      <img src="/water-ball.gif" alt="Water ball" className="water-ball slow" style={{right: `${xPositionSlow}%`, top: `calc(${yPositionSlow}% - 40px)`}} id="water-ball-slow"/>
     </div>
   );
 }
