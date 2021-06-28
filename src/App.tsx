d88ff419358211f9c68e9c20e469edbf80f5bfae
vinassefranche <vincent.francois@inato.com>
2021-06-28 17:51:51 +0200

    Add best score storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Game } from "./Game";
+import { useBestScore } from "./useBestScore";
 
 const App = () => {
   const [gameOver, setGameOver] = useState(false);
   const [score, setScore] = useState(0);
+  const { bestScore, checkAndSetBestScore, resetBestScore } = useBestScore();
   return gameOver ? (
     <div className="game-over-menu">
       <img src="/explosion.png" alt="Explosion!"></img>
       <div className="game-over">Game Over!</div>
       <span className="score">Score: {score}</span>
-      <div>
+      <span className="score">Best score: {bestScore}</span>
+      <div className="buttons">
         <button
           onClick={() => {
             setGameOver(false);
@@ -19,12 +22,16 @@ const App = () => {
         >
           Restart
         </button>
+        <button onClick={resetBestScore}>Reset best score</button>
       </div>
     </div>
   ) : (
     <div className="game-container">
       <Game
-        setGameOver={() => setGameOver(true)}
+        setGameOver={() => {
+          setGameOver(true);
+          checkAndSetBestScore(score);
+        }}
         score={score}
         setScore={setScore}
       />
---
+++ src/useBestScore.ts
@@ -0,0 +1,35 @@
+import { useCallback, useState } from "react";
+
+const LOCAL_STORAGE_KEY = "best-score";
+
+export const useBestScore = () => {
+  const [bestScore, setBestScore] = useState<number>(getStoredBestScore());
+
+  const checkAndSetBestScore = useCallback(
+    (score: number) => {
+      if (score > bestScore) {
+        setBestScore(score);
+        localStorage.setItem(LOCAL_STORAGE_KEY, score.toString());
+      }
+    },
+    [setBestScore, bestScore]
+  );
+
+  return {
+    bestScore,
+    checkAndSetBestScore,
+    resetBestScore: () => setBestScore(0),
+  };
+};
+
+const getStoredBestScore = () => {
+  const storedBestScore = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (storedBestScore === null) {
+    return 0;
+  }
+  try {
+    return JSON.parse(storedBestScore);
+  } catch (error) {
+    return 0;
+  }
+};
